28d61b81a0b7d868574d24c04458e99eef2194ea
azizmoubsit <azizmobsit@gmail.com>
2024-06-28 21:31:17 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const https = require("https");
+const fs = require("fs");
+const path = require("path");
+const app = express();
+
+app.use("/", (req, res, next) => {
+  res.send("hello I am SSL Server !");
+});
+
+const options = {
+  key: fs.readFileSync(path.join(__dirname, "./key.pem")),
+  cert: fs.readFileSync(path.join(__dirname, "./cert.pem")),
+};
+
+const sslServer = https.createServer(options, app);
+sslServer.listen(1337, () => {
+  console.log("Secure server is listening on port 1337");
+});
